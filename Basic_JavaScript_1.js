66a729fa7c3479449285ac703b43283046dc6d16
Maksims Sersnovs <maksimssir@gmail.com>
2022-10-31 20:33:17 +0000

    Basic HTML 1
    Basic HTML 1

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert ("Hello my page!");
+script>
+	function My_first_Function() {
+		var String = "Kiss me, I'm Irish";
+		var result = String.fontcolor("green");
+		document.getElementByID("Irish").innerHTML =
+		result;
+		}
+</script>
