a30f945df215de270e97b9d8a51045ca5f55ca3a
Maksims Sersnovs <maksimssir@gmail.com>
2023-01-31 14:34:46 +0000

    changes

--- Assignment 1/Basic_JavaScript_1.js
+++
@@ -1,9 +0,0 @@
-alert ("Hello my page!");
-script>
-	function My_first_Function() {
-		var String = "Kiss me, I'm Irish";
-		var result = String.fontcolor("green");
-		document.getElementByID("Irish").innerHTML =
-		result;
-		}
-</script>
